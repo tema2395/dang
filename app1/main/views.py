9f6a0a741d3157d8d280ff143f58eb38649554e2
tema2395 <f0ul2212@mail.ru>
2024-03-26 16:18:31 +0300

    make views.py(main_page, about_page)

--- app1/main/views.py
+++ app1/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Home page")
+
+
+def about(request):
+    return HttpResponse("About page")
