d2e8e91a0891552e23bc60b6d6f05dc6d25839a5
tema2395 <f0ul2212@mail.ru>
2024-03-26 17:43:53 +0300

    first index.html and uprade gitignore

--- app1/main/views.py
+++ app1/main/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Home page")
+    context = {
+        "title": "Home", 
+        "content": "Главная страница магазина - Home"
+        }
+    return render(request, "main/index.html", context)
 
 
 def about(request):
